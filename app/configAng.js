202d8beb3ff08d92adaf7e176b3534529f408114
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-11 16:04:47 -0500

    basic angular setup

---
+++ app/configAng.js
@@ -0,0 +1,35 @@
+//assign the angular module to the var app
+var app = angular.module("similarTastesApp", ['ngRoute']);
+
+//configure "app" with routeProvider
+app.config(($routeProvider)=> {
+  $routeProvider
+    //when at the base page
+    .when("/", {
+      //use the Home Ctrl
+      controller: "homeCtrl",
+      //use the partial "home"
+      templateUrl : "partials/home.html"
+    })
+    .when("/input", {
+      // use the add product ctrl
+      controller: "formCtrl",
+      //use the partial "addProduct"
+      templateUrl: "partials/form.html"
+    })
+    .when("/restaurant", {
+      // use the add product ctrl
+      controller: "restaurantCtrl",
+      //use the partial "addProduct"
+      templateUrl: "partials/restaurantInfo.html"
+    })
+    .when("/about", {
+      // use the add product ctrl
+      controller: "aboutCtrl",
+      //use the partial "addProduct"
+      templateUrl: "partials/about.html"
+    })
+    .otherwise({
+      redirectTo: "/"
+    })
+});

e97df3b9e0e2c6475e8467bda7e8241627163758
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-12 11:55:23 -0500

    can call function to get similar reviews

--- app/factories/getReviewsFact.js
+++ app/factories/getReviewsFact.js
@@ -4,9 +4,10 @@ app.factory('getReviewsFactory', function($http){
       //get all the products from api
       let restaurant_id = user.restaurant_id;
       let rating = user.rating;
+      console.log("rating", user.rating)
       let restaurant_to_compare = user.restaurant_to_compare;
       return $http
-      .get(`http://localthost:3000/api/v1/reviews/filtered?restaurant_id=${restaurant_id}&rating=${rating}&restaurant_to_compare=${restaurant_to_compare}`)
+      .get(`http://localhost:3000/api/v1/reviews/filtered?restaurant_id=${restaurant_id}&rating=${rating}&restaurant_to_compare=${restaurant_to_compare}`)
       //parse the return from api, just returning the data object
       .then((e)=>{
         console.log("e in reviews factory", e)
